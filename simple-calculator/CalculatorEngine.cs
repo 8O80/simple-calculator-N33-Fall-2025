ab8bf16ab697cc94354b5ed8a1aa30b6c72fd610
musta <Mustafakhafajy10@gmail.com>
2025-09-12 13:12:29 -0400

    Updated CalculaterEngine.cs

--- simple-calculator/CalculatorEngine.cs
+++ simple-calculator/CalculatorEngine.cs
@@ -9,9 +9,31 @@ namespace SimpleCalculator
             //You can use switch case also here for multiple operations or use if-else-if ladder
             double result = 0;
 
-            if (argOperation == "+" || argOperation.ToLower() == "add")
+            switch (argOperation.ToLower())
             {
-                result = argFirstNumber + argSecondNumber;
+                case "+":
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+
+                case "-":
+                case "subtract":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+
+                case "*":
+                case "multiply":
+                case "x":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+
+                case "/":
+                case "divide":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+
+                default:
+                    throw new InvalidOperationException("Invalid operation.");
             }
 
             return result;
